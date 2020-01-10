f251f7154bce4dce3fea91399f626732483cae6e
JLBicho <jlmv.96@gmail.com>
2020-01-10 12:26:43 +0100

    Cambios en cargarModeloYUsar
    Ahora coge todos los clasificadores que haya en la carpeta y los muestra con sus resultados

--- identExpresiones/cargarModeloYUsar.py
+++ identExpresiones/cargarModeloYUsar.py
@@ -12,6 +12,7 @@ import numpy as np
 import pandas as pd
 import pickle
 import cv2
+import os
 
 # Devuelve la imágen a partir del dataframe
 def cargarImagen(datos):
@@ -25,27 +26,30 @@ def marcarCara(imagen, detector, marcador):
     return puntos[0]
 
 if __name__ == "__main__":
-    filename = 'ModelosFelicidad(80-20)/' + 'pasivoAgresivoClassifier [0.927].sav'
-    loaded_model = pickle.load(open(filename, 'rb'))
+    path = 'ModelosFelicidad(80-20)/'
+    classifiers = os.listdir(path)
+    for classifier in classifiers:
+        filename = 'ModelosFelicidad(80-20)/' + classifier
+        loaded_model = pickle.load(open(filename, 'rb'))
 
-    datos = pd.read_csv('Database_nosotros/dataBaseAUs.csv')
-    X = datos.iloc[:, 1:]
-    y = datos.Feliz
+        datos = pd.read_csv('Database_nosotros/dataBaseAUs.csv')
+        X = datos.iloc[:, 1:]
+        y = datos.Feliz
 
-    test = np.array(X.iloc[:,:])
-    
-    test.reshape(-1,1)
-    result = loaded_model.predict(test)
-    good = 0;
-    bad = 0;
-    total = 0;
-    for i in range(0,len(result)):
-        if result[i] == y[i]:
-            good = good + 1
-        else:
-            bad = bad + 1
+        test = np.array(X.iloc[:,:])
+        
+        test.reshape(-1,1)
+        result = loaded_model.predict(test)
+        good = 0;
+        bad = 0;
+        total = 0;
+        for i in range(0,len(result)):
+            if result[i] == y[i]:
+                good = good + 1
+            else:
+                bad = bad + 1
 
-        total = total + 1 
+            total = total + 1 
 
-    print("good = "+ str(good) + ", bad = "+str(bad) + ", total = " + str(total))
-    print("good(%) = "+ str(round(good/total*100,1)) + ", bad(%) = " + str(round(bad/total*100,1)))
+        print(classifier)
+        print("good(%) = "+ str(round(good/total*100,1)) + ", bad(%) = " + str(round(bad/total*100,1)))
